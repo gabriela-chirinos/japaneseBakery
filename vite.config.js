01baf4e9d80a8472eee3f42d93fe96f76ecc0c8b
Gabriela-Chirinos <chirinos@outlook.com>
2026-04-06 11:35:58 -0700

    updated config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/JAPANESEBAKERY/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './tests/setup.js',
-    base: 'https://github.com/gabriela-chirinos/japaneseBakery',
   },
 })
